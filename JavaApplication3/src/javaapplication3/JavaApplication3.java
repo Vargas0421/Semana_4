bed890a442f9df2a4d9c0fb0657dbb043f254fdc
Vargas0421 <136260759+Vargas0421@users.noreply.github.com>
2023-10-04 20:47:51 -0600

    Add files via upload

---
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author LABORATORIO 04
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        calculadora calc = new calculadora();
+        System.err.println("la suma de  5 + 4 es:" + calc.sunaDosNumeros(5, 4));
+    }
+    
+}
---
+++ JavaApplication3/src/javaapplication3/calculadora.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author LABORATORIO 04
+ */
+public class calculadora {
+    
+    public double sunaDosNumeros (double a, double b){
+        return a + b;
+        }
+    public double restaDosNumeros (double a, double b){
+        return a - b;
+        }
+     public double divideDosNumeros (double a, double b){
+        return a / b;
+        }
+    public double multipicaDosNumeros (double a, double b){
+        return a * b;
+        }
+}
